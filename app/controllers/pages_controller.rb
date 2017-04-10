6f8c56b1457ca6b1d21344a18789f75899247a86
Azbagheri <azadeh.bagheri7@gmail.com>
2017-04-10 15:43:54 -0400

    Adding header to the pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,14 @@
 class PagesController < ApplicationController
 
 def welcome
-
+ @header = "This is the welcome page"
 end
 
 def about
 end
 
 def contest
+  @header = "This is the contest page"
 end
 
 end
